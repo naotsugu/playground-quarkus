7d110eabf2138638ba2d64e26d296cda42d32dc0
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-03-30 19:58:25 +0900

    build: added rest-simple

---
+++ rest-simple/src/main/java/com/mammb/GreetingService.java
@@ -0,0 +1,10 @@
+package com.mammb;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+}
